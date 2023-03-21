09aaf368eb603469fb187d1a47ff4becb933597c
vette243 <liuyiyang1997@hotmail.com>
2023-03-21 00:50:12 -0400

    update existing geojson

--- script.js
+++ script.js
@@ -28,17 +28,14 @@ Step 2: VIEW GEOJSON POINT DATA ON MAP
 //      Convert the response to JSON format and then store the response in your new variable
 let Pedgeojson
 
-fetch('https://raw.githubusercontent.com/smith-lg/ggr472-wk6-demo/main/data/torontomusicvenues.geojson')
+fetch('https://github.com/vette243/GGR472_Lab4/blob/6ee44410f22684927871406c39135e658ae47dd5/pedcyc_collision_06-21.geojson')
     .then(response => response.json())
     .then(response => {
         console.log(response); //Check response in console
         Pedgeojson = response; // Store geojson as variable using URL from fetch response
     });
 
-let geojson = {
-    'type': 'FeatureCollection',
-        'features': []
-    };
+
 
 /*--------------------------------------------------------------------
     Step 3: CREATE BOUNDING BOX AND HEXGRID
@@ -51,12 +48,12 @@ map.on('load', () => {
     //Add datasource using GeoJSON variable
     map.addSource('Pedgeojson', {
         type: 'geojson',
-        data: geojson
+        data: 'https://vette243.github.io/GGR472_Lab4/pedcyc_collision_06-21.geojson'
     });
 
     //Set style for when new points are added to the data source
     map.addLayer({
-        'id': 'input-pnts',
+        'id': 'Collision',
         'type': 'point',
         'source': 'Pedgeojson',
         'paint': {
@@ -64,8 +61,6 @@ map.on('load', () => {
             'circle-color': 'blue'
         }
     });
-
-
 });
 
 
@@ -74,9 +69,14 @@ Step 4: AGGREGATE COLLISIONS BY HEXGRID
 --------------------------------------------------------------------*/
 //HINT: Use Turf collect function to collect all '_id' properties from the collision points data for each heaxagon
 //      View the collect output in the console. Where there are no intersecting points in polygons, arrays will be empty
-
-
-
+const collectedIds = [];
+Pedgeojson.features.forEach(() => {
+    // get the intersection points between the hexagon and the collision points
+    const intersectingPoints = turf.collect(Pedgeojson, '_id');
+collectedIds.push(intersectingPoints.features.map((f) => f.properties._id));
+});
+// view the collected IDs in the console
+console.log(collectedIds);
 // /*--------------------------------------------------------------------
 // Step 5: FINALIZE YOUR WEB MAP
 // --------------------------------------------------------------------*/
@@ -87,4 +87,14 @@ Step 4: AGGREGATE COLLISIONS BY HEXGRID
 //        - The maximum number of collisions found in a hexagon
 //      Add a legend and additional functionality including pop-up windows
 
-
+map.on('click',(e) =>{
+    const clickedpoint = {
+        'type': 'Feature',
+        'geometry':{
+            'type':'Point',
+            'coordinates':[e.lngLat.lng,e.lngLat.lat]
+        }
+    };
+    //Add clicked point to previously empty geojson eature Collection
+    geojson.features.push(clickedpoint);
+})
